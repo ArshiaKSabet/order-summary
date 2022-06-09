2666a668cfae9eca3c9a1f1df014226fbbb77de2
Eddie Bucht <bucht0289@gmail.com>
2022-06-09 23:11:25 +0200

    added card and the banner

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from "react";
+
+const card = () => {
+  return (
+    <body className="card-container">
+      <div className="card">
+        <img
+          className="banner"
+          src="/images/illustration-hero.svg"
+          alt="banner"
+        />
+      </div>
+    </body>
+  );
+};
+
+export default card;
